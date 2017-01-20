ad4df726bf32f581cc3ca04ce3938d122efdaf04
Meadows <meadom@amazon.com>
2017-01-19 16:06:28 -0800

    Starting to sketch out object structure. Next I should define interfaces/abstractclasses/method signatures

---
+++ Libertalia/src/Pirates/Parrot.scala
@@ -0,0 +1,5 @@
+package Pirates
+
+class Parrot {
+  // This is the lowest rank pirate... need to implement parent stuff
+}
---
+++ Libertalia/src/Pirates/Pirate.scala
@@ -0,0 +1,14 @@
+package Pirates
+
+class Pirate {
+  // This should be an abstract class soon
+  /*
+   * Include the following things (more or less)
+   * 
+   * getRank()
+   * performDaytimeActivity()
+   * performNightActivity()
+   * performEndOfVoyageActivity()
+   * 
+   */
+}
