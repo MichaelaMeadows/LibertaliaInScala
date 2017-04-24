77cf87171cdbcee307a8a773b6609d02e897c7e0
Meadows <meadom@amazon.com>
2017-04-23 21:28:49 -0700

    Work on the aprrot and mutineer

--- Libertalia/src/Pirates/Parrot.scala
+++ Libertalia/src/Pirates/Parrot.scala
@@ -7,8 +7,9 @@ class Parrot(owningPlayer:Int) extends Pirate(1, owningPlayer) {
   
    val rankOrdering:List[Int] = List(1,2,3,4,5,6);
 
-    /*
+   /*
    * Swap with another pirate in hand
+   * Logic lives in the game state class and is executed before daytime technically starts.
    */
    def dayActivity(state: GameState) {
    }
